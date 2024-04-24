094b21f50e8999e0de257fb3b9f327e78fa5a9bb
Isac pacheco <isac.pachecolopez@unicolombo.edu.co>
2024-04-24 16:40:44 -0500

    Filechooser

---
+++ guia_14/src/jfilechoouser/Jfilechoouser.java
@@ -0,0 +1,20 @@
+
+package jfilechoouser;
+
+/**
+ *
+ * @author isac pacheco
+ */
+public class Jfilechoouser {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Principal principal = new Principal();
+        principal.setLocationRelativeTo(null);
+        principal.setVisible(true);
+    }
+    
+}
